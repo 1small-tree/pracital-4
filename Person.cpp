c18015bc3858cb4e18d5019767a3529a15a3e7dd
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-09-02 20:36:15 +0930

    Add files via upload

--- Person.cpp
+++ Person.cpp
@@ -1,31 +1,57 @@
-#include "Person.h"
+#include <iostream>
 
-Person::Person()
+using namespace std;
 
-{
-
-this->myName = "?";
-
-this->salary = 0;
-
-}
-
-string Person::getName(){ return this->myName; }
-
-void Person::setName(string name){ this->myName = name; }
-
-int Person::getSalary(){ return this->salary; }
-
-void Person::setSalary(int salary){ this->salary = salary; }
-
-string Person::toString()
+class Person
 
 {
 
-stringstream ss;
+private:
 
-ss << "Name: " << getName() << ", Salary: $" << getSalary();
+string name;
 
-return ss.str();
+int salary;
+
+public:
+
+// Parameterized Constructor
+
+Person(string n, int s)
+
+{
+
+name = n;
+
+salary = s;
 
 }
+
+string getName()
+
+{
+
+return name;
+
+}
+
+void setName(string myName){
+
+name = myName;
+
+}
+
+int getSalary()
+
+{
+
+return salary;
+
+}
+
+void setSalary(int mySalary){
+
+salary = mySalary;
+
+}
+
+};
