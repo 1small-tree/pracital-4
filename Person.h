560934f761b9f77c4647fb790b03b7a39b270bb4
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-09-02 19:47:24 +0930

    Add files via upload

---
+++ Person.h
@@ -0,0 +1,28 @@
+#include "Capybara.h"
+
+class Person
+
+{
+
+private:
+
+string myName;
+
+int salary;
+
+public:
+
+Person(string, int);
+
+void setName(string);
+
+string getName();
+
+void setSalary(int);
+
+int getSalary();
+
+string toString();
+
+};
+
